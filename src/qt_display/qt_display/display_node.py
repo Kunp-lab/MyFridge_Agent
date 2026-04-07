e83249911b8be78cb5cf565d4884be66f4542cdd
kunp <1709038900@qq.com>
2026-04-07 12:52:58 +0800

    the task of recommend func finished

--- src/qt_display/qt_display/display_node.py
+++ src/qt_display/qt_display/display_node.py
@@ -315,6 +315,7 @@ class DisplayNode(Node, QObject):
             ],
             response_format={"type": "json_object"},
             max_tokens=4000,
+            temperature=0.1,
         )
 
         try:
